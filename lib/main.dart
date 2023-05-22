f740149e5d9b64770431051bceca846ab3b39d41
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-22 21:42:41 +0300

    ADD: Added the app to only accept profile potrait up or down

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gdsc_bloc/Blocs/AuthenticationBloc/authentication_bloc.dart';
 import 'package:gdsc_bloc/Blocs/Network/network_bloc.dart';
@@ -14,6 +15,11 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
 
   runApp(const MyApp());
 }
