3999a4d3a351022783414b0a50f105106cc4c5e6
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-22 13:13:57 +0300

    ADD: Added the start and the end time for the event model

--- lib/Data/Models/event_model.dart
+++ lib/Data/Models/event_model.dart
@@ -4,34 +4,38 @@
 
 import 'dart:convert';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 Event eventFromJson(String str) => Event.fromJson(json.decode(str));
 
 String eventToJson(Event data) => json.encode(data.toJson());
 
 class Event {
-    String? title;
-    String? venue;
-    String? time;
-    String? organizers;
-    String? link;
-    String? imageUrl;
-    String? description;
-    String? portfolio;
-    String? date;
+  String? title;
+  String? venue;
+  String? time;
+  String? organizers;
+  String? link;
+  String? imageUrl;
+  String? description;
+  Timestamp startTime;
+  Timestamp endTime;
+  String? date;
 
-    Event({
-        this.title,
-        this.venue,
-        this.time,
-        this.organizers,
-        this.link,
-        this.imageUrl,
-        this.description,
-        this.portfolio,
-        this.date,
-    });
+  Event({
+    this.title,
+    this.venue,
+    this.time,
+    this.organizers,
+    this.link,
+    this.imageUrl,
+    this.description,
+    this.date,
+    required this.startTime,
+    required this.endTime,
+  });
 
-    factory Event.fromJson(Map<String, dynamic> json) => Event(
+  factory Event.fromJson(Map<String, dynamic> json) => Event(
         title: json["title"],
         venue: json["venue"],
         time: json["time"],
@@ -39,11 +43,12 @@ class Event {
         link: json["link"],
         imageUrl: json["imageUrl"],
         description: json["description"],
-        portfolio: json["portfolio"],
         date: json["date"],
-    );
+        startTime: json["startTime"],
+        endTime: json["endTime"],
+      );
 
-    Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toJson() => {
         "title": title,
         "venue": venue,
         "time": time,
@@ -51,7 +56,8 @@ class Event {
         "link": link,
         "imageUrl": imageUrl,
         "description": description,
-        "portfolio": portfolio,
         "date": date,
-    };
+        "startTime": startTime,
+        "endTime": endTime,
+      };
 }
