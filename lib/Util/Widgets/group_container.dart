8818a9e57bfe1bbb19070a9675fa7ed1fbb3606e
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-27 14:55:25 +0300

    REFACTOR: Replaced the container image with a cache network image

--- lib/Util/Widgets/group_container.dart
+++ lib/Util/Widgets/group_container.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -17,46 +18,68 @@ class GroupContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          margin: const EdgeInsets.only(right: 8),
-          height: height * 0.12,
-          width: height * 0.13,
-          decoration: BoxDecoration(
-            border: Border.all(
-              width: 0.3,
-              color: Colors.black54,
-            ),
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              image: NetworkImage(
-                image,
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CachedNetworkImage(
+            height: height * 0.12,
+            width: width * 0.28,
+            placeholder: (context, url) {
+              return Container(
+                height: height * 0.1,
+                width: width * 0.28,
+                decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 243, 243, 243),
+                    borderRadius: BorderRadius.circular(10)),
+              );
+            },
+            errorWidget: ((context, url, error) {
+              return const Icon(
+                Icons.error,
+                size: 20,
+                color: Colors.red,
+              );
+            }),
+            imageUrl: image,
+            fit: BoxFit.fitHeight,
+            imageBuilder: (context, imageProvider) {
+              return AnimatedContainer(
+               
+                decoration: BoxDecoration(
+                  // shape: BoxShape.circle,
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(width: 0.4, color: const Color(0xff666666)),
+                  image: DecorationImage(
+                    image: imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                duration: const Duration(milliseconds: 500),
+              );
+            },
+          ),
+          const SizedBox(height: 5),
+          Center(
+            child: SizedBox(
+              width: width * 0.27,
+              child: Text(
+                title,
+                overflow: TextOverflow.clip,
+                maxLines: 2,
+                style: GoogleFonts.inter(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
               ),
-              fit: BoxFit.cover,
             ),
           ),
-        ),
-        const SizedBox(height: 5),
-        Center(
-          child: SizedBox(
-            width: width * 0.27,
-            child: Text(
-              title,
-              overflow: TextOverflow.clip,
-              maxLines: 2,
-              style: GoogleFonts.inter(
-                fontWeight: FontWeight.w500,
-                fontSize: 12,
-                color: Colors.black,
-              ),
-            ),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
