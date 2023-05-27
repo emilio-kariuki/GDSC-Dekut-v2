4335ceec174108ca26493833b948a9db190c4f1d
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-27 14:59:34 +0300

    ADD: Added the refresh indicator widget to refresh user information

--- lib/View/Profile/Account/personal_information_page.dart
+++ lib/View/Profile/Account/personal_information_page.dart
@@ -4,7 +4,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:gdsc_bloc/Blocs/AppFuntions/app_functions_cubit.dart';
-import 'package:gdsc_bloc/Data/providers.dart';
+import 'package:gdsc_bloc/Data/Repository/providers.dart';
 import 'package:gdsc_bloc/Util/Widgets/input_field.dart';
 import 'package:gdsc_bloc/Util/Widgets/loading_circle.dart';
 import 'package:gdsc_bloc/Util/image_urls.dart';
@@ -131,160 +131,165 @@ class _PersonalInformationState extends State<PersonalInformation> {
           ),
           backgroundColor: Colors.white,
           body: SafeArea(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Center(
-                      child: Stack(
-                        children: [
-                          Container(
-                            height: height * 0.13,
-                            width: height * 0.13,
-                            decoration: const BoxDecoration(
-                              color: Color(0xffF6F6F6),
-                              shape: BoxShape.circle,
-                            ),
-                            child: BlocBuilder<AppFunctionsCubit,
-                                AppFunctionsState>(
-                              builder: (context, state) {
-                                if (state is ImagePicked) {
-                                  image = state.imageUrl;
-                                  ClipRRect(
-                                    borderRadius: BorderRadius.circular(100),
-                                    child: Image.file(
-                                      state.image,
-                                      fit: BoxFit.fitHeight,
-                                      height: height * 0.13,
-                                      width: height * 0.13,
-                                    ),
-                                  );
-                                } else {
-                                  return Center(
-                                      child: CachedNetworkImage(
-                                    imageUrl: image,
-                                    imageBuilder: (context, imageProvider) {
-                                      return Container(
+            child: RefreshIndicator(
+               onRefresh: () async {
+                  await fetchUser();
+                },
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Center(
+                        child: Stack(
+                          children: [
+                            Container(
+                              height: height * 0.13,
+                              width: height * 0.13,
+                              decoration: const BoxDecoration(
+                                color: Color(0xffF6F6F6),
+                                shape: BoxShape.circle,
+                              ),
+                              child: BlocBuilder<AppFunctionsCubit,
+                                  AppFunctionsState>(
+                                builder: (context, state) {
+                                  if (state is ImagePicked) {
+                                    image = state.imageUrl;
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(100),
+                                      child: Image.file(
+                                        state.image,
+                                        fit: BoxFit.fitHeight,
                                         height: height * 0.13,
                                         width: height * 0.13,
-                                        decoration: BoxDecoration(
-                                          color: const Color(0xffF6F6F6),
-                                          shape: BoxShape.circle,
-                                          border: Border.all(
-                                            color: Colors.grey[500]!,
-                                            width: 0.7,
+                                      ),
+                                    );
+                                  } else {
+                                    return Center(
+                                        child: CachedNetworkImage(
+                                      imageUrl: image,
+                                      imageBuilder: (context, imageProvider) {
+                                        return Container(
+                                          height: height * 0.13,
+                                          width: height * 0.13,
+                                          decoration: BoxDecoration(
+                                            color: const Color(0xffF6F6F6),
+                                            shape: BoxShape.circle,
+                                            border: Border.all(
+                                              color: Colors.grey[500]!,
+                                              width: 0.7,
+                                            ),
+                                            image: DecorationImage(
+                                              image: imageProvider,
+                                              fit: BoxFit.cover,
+                                            ),
                                           ),
-                                          image: DecorationImage(
-                                            image: imageProvider,
-                                            fit: BoxFit.cover,
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ));
-                                }
-                                return const Center(
-                                  child: Icon(
-                                    Icons.person,
-                                    size: 50,
-                                    color: Colors.grey,
-                                  ),
-                                );
-                              },
-                            ),
-                          ),
-                          Positioned.fill(
-                              child: Align(
-                            alignment: Alignment.bottomRight,
-                            child: Semantics(
-                              button: true,
-                              child: InkWell(
-                                onTap: () {
-                                  context.read<AppFunctionsCubit>().getImage();
-                                },
-                                child: Container(
-                                  height: 30,
-                                  width: 30,
-                                  padding: const EdgeInsets.all(2),
-                                  decoration: const BoxDecoration(
-                                    color: Colors.blue,
-                                    shape: BoxShape.circle,
-                                  ),
-                                  child: const Center(
+                                        );
+                                      },
+                                    ));
+                                  }
+                                  return const Center(
                                     child: Icon(
-                                      Icons.photo_camera_outlined,
-                                      color: Colors.white,
-                                      size: 17,
+                                      Icons.person,
+                                      size: 50,
+                                      color: Colors.grey,
+                                    ),
+                                  );
+                                },
+                              ),
+                            ),
+                            Positioned.fill(
+                                child: Align(
+                              alignment: Alignment.bottomRight,
+                              child: Semantics(
+                                button: true,
+                                child: InkWell(
+                                  onTap: () {
+                                    context.read<AppFunctionsCubit>().getImage();
+                                  },
+                                  child: Container(
+                                    height: 30,
+                                    width: 30,
+                                    padding: const EdgeInsets.all(2),
+                                    decoration: const BoxDecoration(
+                                      color: Colors.blue,
+                                      shape: BoxShape.circle,
+                                    ),
+                                    child: const Center(
+                                      child: Icon(
+                                        Icons.photo_camera_outlined,
+                                        color: Colors.white,
+                                        size: 17,
+                                      ),
                                     ),
                                   ),
                                 ),
                               ),
-                            ),
-                          ))
-                        ],
+                            ))
+                          ],
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    InputField(
-                      title: "Name",
-                      controller: nameController,
-                      hintText: "Enter your name",
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    InputField(
-                      title: "Email",
-                      controller: emailController,
-                      hintText: "Enter your email",
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    InputField(
-                      title: "Phone",
-                      controller: phoneController,
-                      hintText: "Enter your phone",
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    InputField(
-                      title: "Github",
-                      controller: githubController,
-                      hintText: "Enter your github username",
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    InputField(
-                      title: "Twitter",
-                      controller: twitterController,
-                      hintText: "Enter your twitter username",
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    InputField(
-                      title: "Technology",
-                      controller: technologyController,
-                      hintText: "Enter your technology",
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    InputField(
-                      title: "Linkedin",
-                      controller: linkedinController,
-                      hintText: "Enter your linkedin  name",
-                    ),
-                  ],
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      InputField(
+                        title: "Name",
+                        controller: nameController,
+                        hintText: "Enter your name",
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InputField(
+                        title: "Email",
+                        controller: emailController,
+                        hintText: "Enter your email",
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InputField(
+                        title: "Phone",
+                        controller: phoneController,
+                        hintText: "Enter your phone",
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InputField(
+                        title: "Github",
+                        controller: githubController,
+                        hintText: "Enter your github username",
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InputField(
+                        title: "Twitter",
+                        controller: twitterController,
+                        hintText: "Enter your twitter username",
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InputField(
+                        title: "Technology",
+                        controller: technologyController,
+                        hintText: "Enter your technology",
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InputField(
+                        title: "Linkedin",
+                        controller: linkedinController,
+                        hintText: "Enter your linkedin  name",
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
