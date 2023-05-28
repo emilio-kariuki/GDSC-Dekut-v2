870a6b4e27bf00ad33602a333ef219cc93d03202
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-28 03:38:10 +0300

    ADD: Increased the height and removed the padding to the image

--- lib/Util/Widgets/group_container.dart
+++ lib/Util/Widgets/group_container.dart
@@ -18,68 +18,65 @@ class GroupContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(5.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CachedNetworkImage(
-            height: height * 0.12,
-            width: width * 0.28,
-            placeholder: (context, url) {
-              return Container(
-                height: height * 0.1,
-                width: width * 0.28,
-                decoration: BoxDecoration(
-                    color: const Color.fromARGB(255, 243, 243, 243),
-                    borderRadius: BorderRadius.circular(10)),
-              );
-            },
-            errorWidget: ((context, url, error) {
-              return const Icon(
-                Icons.error,
-                size: 20,
-                color: Colors.red,
-              );
-            }),
-            imageUrl: image,
-            fit: BoxFit.fitHeight,
-            imageBuilder: (context, imageProvider) {
-              return AnimatedContainer(
-               
-                decoration: BoxDecoration(
-                  // shape: BoxShape.circle,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 0.4, color: const Color(0xff666666)),
-                  image: DecorationImage(
-                    image: imageProvider,
-                    fit: BoxFit.cover,
-                  ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        CachedNetworkImage(
+          height: height * 0.13,
+          width: width * 0.33,
+          placeholder: (context, url) {
+            return Container(
+              height: height * 0.1,
+              width: width * 0.33,
+              decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 243, 243, 243),
+                  borderRadius: BorderRadius.circular(10)),
+            );
+          },
+          errorWidget: ((context, url, error) {
+            return const Icon(
+              Icons.error,
+              size: 20,
+              color: Colors.red,
+            );
+          }),
+          imageUrl: image,
+          fit: BoxFit.fitHeight,
+          imageBuilder: (context, imageProvider) {
+            return AnimatedContainer(
+             
+              decoration: BoxDecoration(
+                // shape: BoxShape.circle,
+                borderRadius: BorderRadius.circular(10),
+                border: Border.all(width: 0.4, color: const Color(0xff666666)),
+                image: DecorationImage(
+                  image: imageProvider,
+                  fit: BoxFit.cover,
                 ),
-                duration: const Duration(milliseconds: 500),
-              );
-            },
-          ),
-          const SizedBox(height: 5),
-          Center(
-            child: SizedBox(
-              width: width * 0.27,
-              child: Text(
-                title,
-                overflow: TextOverflow.clip,
-                maxLines: 2,
-                style: GoogleFonts.inter(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 12,
-                  color: Colors.black,
-                ),
+              ),
+              duration: const Duration(milliseconds: 500),
+            );
+          },
+        ),
+        const SizedBox(height: 5),
+        Center(
+          child: SizedBox(
+            width: width * 0.27,
+            child: Text(
+              title,
+              overflow: TextOverflow.clip,
+              maxLines: 2,
+              style: GoogleFonts.inter(
+                fontWeight: FontWeight.w500,
+                fontSize: 12,
+                color: Colors.black,
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
