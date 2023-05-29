1510874db9b4507d10ca460234b90f9e97d01432
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-29 03:51:35 +0300

    REFACTOR: Made all the fields required

--- lib/Data/Models/message_model.dart
+++ lib/Data/Models/message_model.dart
@@ -9,21 +9,24 @@ Message messageFromJson(String str) => Message.fromJson(json.decode(str));
 String messageToJson(Message data) => json.encode(data.toJson());
 
 class Message {
-  String? name;
-  String? id;
-  String? message;
-  int? time;
-  DateTime ?timestamp;
+  String name;
+  String id;
+  String message;
+  int time;
+  String image;
+  DateTime timestamp;
 
   Message({
-    this.name,
-    this.id,
-    this.message,
-    this.time,
-    this.timestamp
+    required this.name,
+    required this.id,
+    required this.message,
+    required this.time,
+    required this.timestamp,
+    required this.image,
   });
 
   factory Message.fromJson(Map<String, dynamic> json) => Message(
+        image: json["image"],
         name: json["name"],
         id: json["id"],
         message: json["message"],
@@ -32,10 +35,11 @@ class Message {
       );
 
   Map<String, dynamic> toJson() => {
+        "image": image,
         "name": name,
         "id": id,
         "message": message,
         "time": time,
-        "timestamp": timestamp!.toIso8601String()
+        "timestamp": timestamp.toIso8601String()
       };
 }
