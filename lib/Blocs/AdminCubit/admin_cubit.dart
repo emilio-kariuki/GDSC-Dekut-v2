6081b52eeefa6c0e312db8bd27a061d4d6057a3c
Emilio Kariuki <emilio113kariuki@gmail.com>
2023-05-30 16:10:53 +0300

    ADD: Added the admin cubit to check the user status is they are an admin or not

---
+++ lib/Blocs/AdminCubit/admin_cubit.dart
@@ -0,0 +1,22 @@
+import 'package:bloc/bloc.dart';
+import 'package:gdsc_bloc/Data/Repository/providers.dart';
+
+part 'admin_state.dart';
+
+class AdminCubit extends Cubit<AdminState> {
+  AdminCubit() : super(AdminInitial());
+
+    void checkUserStatus() async {
+    try {
+      final isAdmin = await Providers().isUserAdmin();
+
+      if (isAdmin) {
+        emit( UserAdmin(isAdmin: true));
+      } else {
+        emit( UserNotAdmin(isAdmin: false));
+      }
+    } catch (e) {
+      emit( UserNotAdmin(isAdmin: false));
+    }
+  }
+}
--- lib/Util/image_urls.dart
+++ lib/Util/image_urls.dart
@@ -13,18 +13,27 @@ class AppImages {
   static const calendar = "Assets/calendar.svg";
   static const clock = "Assets/clock.svg";
   static const messages_black = "Assets/messages_black.svg";
-   static const messages = "Assets/messages.svg";
+  static const messages = "Assets/messages.svg";
   static const messages_white = "Assets/messages_white.svg";
   static const resources_black = "Assets/resources_black.svg";
   static const resources_white = "Assets/resources_white.svg";
   static const search = "Assets/search.svg";
-    static const location = "Assets/location.svg";
-    static const attach = "Assets/attach.svg";
-    static const background = "Assets/whatsapp.png";
+  static const location = "Assets/location.svg";
+  static const attach = "Assets/attach.svg";
+  static const event = "Assets/event.json";
+  static const resources = "Assets/resources.json";
+  static const twitter = "Assets/twitter.json";
+  static const groups = "Assets/groups.json";
+  static const feedback = "Assets/feedback.json";
+  static const problem = "Assets/problem.json";
+  static const leads = "Assets/leads.json";
+  static const developers = "Assets/developers.json";
+  static const announcement = "Assets/announcement.json";
+  static const background = "Assets/whatsapp.png";
   static const eventImage =
       "https://firebasestorage.googleapis.com/v0/b/apt-rite-346310.appspot.com/o/about04.png?alt=media&token=f80825e5-3be5-4e17-817b-cda37d213d29";
   static const oops = "Assets/oops.json";
   static const error = "Assets/error.svg";
-    static const String defaultImage = "https://firebasestorage.googleapis.com/v0/b/apt-rite-346310.appspot.com/o/about02-removebg-preview.png?alt=media&token=f464f698-cfe6-4940-bc24-f6737c7b1a9b";
-
+  static const String defaultImage =
+      "https://firebasestorage.googleapis.com/v0/b/apt-rite-346310.appspot.com/o/about02-removebg-preview.png?alt=media&token=f464f698-cfe6-4940-bc24-f6737c7b1a9b";
 }
